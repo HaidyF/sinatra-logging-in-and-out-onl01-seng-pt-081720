3c335c7544338a0ff942d04234b4e12b2b7ee500
HaidyF <69495135+HaidyF@users.noreply.github.com>
2020-11-17 23:26:07 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user=User.find_by(username: params["username"])
+    session[:id]=@user.id
 
+    redirect to '/account'
+    #erb :
+    
   end
 
   get '/account' do
