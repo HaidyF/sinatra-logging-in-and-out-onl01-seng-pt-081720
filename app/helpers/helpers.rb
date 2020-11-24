1708ad505fa7d6e52c5bc94d187450567c5f5728
HaidyF <69495135+HaidyF@users.noreply.github.com>
2020-11-23 22:02:06 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,6 @@
 class Helpers
     def current_user(session_hash)
-      @current_user = User.find(session_hash[:user_id])
+      current_user = User.find(session_hash[:user_id])
     end
   
     def is_logged_in?(session_hash)
